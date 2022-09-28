40bbdd268b1abdd9465f762e7f5c36313ad8bfd5
Darcy Junior <darcy_junior@yahoo.com.br>
2022-09-28 09:53:10 -0300

    Create 1.multiplos_ifs.py

---
+++ 1.multiplos_ifs.py
@@ -0,0 +1,11 @@
+# Checando multiplos Ifs
+
+nota = 80
+
+if nota == 90 or nota == 80 or nota == 95:
+    print('Parabéns! Você passou no teste 👏👏')
+
+# Melhorando o código
+
+if nota in [90, 80, 95]:
+    print('Parabéns! Você passou no teste 👏👏')
